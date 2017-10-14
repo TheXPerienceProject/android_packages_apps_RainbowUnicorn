d5ccef394a2c4fe6637cccb25f6a2956aff58d86
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2017-10-13 19:55:29 -0500

    some changes :P
    Change-Id: I0db7de5754209916266f3fa424f5c80d762af5d2
    Signed-off-by: klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
    Signed-off-by: klozz <klozz707@gmail.com>

--- src/mx/xperience/rainbowunicorn/RainbowUnicorn.java
+++ src/mx/xperience/rainbowunicorn/RainbowUnicorn.java
@@ -16,11 +16,15 @@
 
 package mx.xperience.rainbowunicorn;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.v7.preference.PreferenceScreen;
+import android.preference.Preference;
+import android.view.Surface;
+import com.android.settings.R;
 
 import com.android.internal.logging.nano.MetricsProto;
-
 import com.android.settings.SettingsPreferenceFragment;
 
 public class RainbowUnicorn extends SettingsPreferenceFragment {
@@ -28,12 +32,41 @@ public class RainbowUnicorn extends SettingsPreferenceFragment {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.xperience_settings_main);
-        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        addPreferencesFromResource(R.xml.xperience_settings);
     }
 
     @Override
-    protected int getMetricsCategory() {
+    public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.RAINBOW_UNICORN;
+    }
+	
+    public static void lockCurrentOrientation(Activity activity) {
+        int currentRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int orientation = activity.getResources().getConfiguration().orientation;
+        int frozenRotation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
+        switch (currentRotation) {
+            case Surface.ROTATION_0:
+                frozenRotation = orientation == Configuration.ORIENTATION_LANDSCAPE
+                        ? ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                        : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                break;
+            case Surface.ROTATION_90:
+                frozenRotation = orientation == Configuration.ORIENTATION_PORTRAIT
+                        ? ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
+                        : ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                break;
+            case Surface.ROTATION_180:
+                frozenRotation = orientation == Configuration.ORIENTATION_LANDSCAPE
+                        ? ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
+                        : ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+                break;
+            case Surface.ROTATION_270:
+                frozenRotation = orientation == Configuration.ORIENTATION_PORTRAIT
+                        ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                        : ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+                break;
+        }
+        activity.setRequestedOrientation(frozenRotation);
     }
 } 

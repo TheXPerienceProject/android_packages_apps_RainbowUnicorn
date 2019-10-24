def16e4ae619fc8cc0bd40abc9e66e80674dac5d
Pranav Vashi <neobuddy89@gmail.com>
2019-10-25 00:58:11 +0530

    Lockscreen Visualizer: Add pulse magic [2/2]
    Change-Id: I7c167b0f52ea4d50c0efc2beed9061c96e4f7296
    Signed-off-by: klozz <klozz@vivaldi.net>

--- src/mx/xperience/rainbowunicorn/fragments/LockScreenSettings.java
+++ src/mx/xperience/rainbowunicorn/fragments/LockScreenSettings.java
@@ -36,19 +36,38 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import mx.xperience.rainbowunicorn.preferences.SecureSettingMasterSwitchPreference;
+
 public class LockScreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer_enabled";
+
+    private SecureSettingMasterSwitchPreference mVisualizerEnabled;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.xperience_lockscreen);
 
+    ContentResolver resolver = getActivity().getContentResolver();
+	        mVisualizerEnabled = (SecureSettingMasterSwitchPreference) findPreference(LOCKSCREEN_VISUALIZER_ENABLED);
+        mVisualizerEnabled.setOnPreferenceChangeListener(this);
+        int visualizerEnabled = Settings.Secure.getInt(resolver,
+                LOCKSCREEN_VISUALIZER_ENABLED, 0);
+        mVisualizerEnabled.setChecked(visualizerEnabled != 0);
+
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
 
+		if (preference == mVisualizerEnabled) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    LOCKSCREEN_VISUALIZER_ENABLED, value ? 1 : 0);
+            return true;
+		}
         return false;
     }
 

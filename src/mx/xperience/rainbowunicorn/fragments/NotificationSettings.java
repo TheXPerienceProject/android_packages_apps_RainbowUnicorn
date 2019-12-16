syberia-project/platform_packages_apps_SyberiaSettings

Add ambient pulse notification [2/2]
Signed-off-by: DennySPB <dennyspb@gmail.com>
Change-Id: I86ab495f83d36925b9173870a95277cc88af802b

src/com/syberia/settings/fragments/NotificationSettings.java
@@ -5,15 +5,22 @@ import com.android.internal.logging.nano.MetricsProto;
 import android.os.Bundle;
 import com.android.settings.R;
 
+import android.content.res.Resources;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class NotificationSettings extends SettingsPreferenceFragment {
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class NotificationSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     private Preference mChargingLeds;
+    private ColorPickerPreference mEdgeLightColorPreference;
+
+    private static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -29,6 +36,37 @@ public class NotificationSettings extends SettingsPreferenceFragment {
                         com.android.internal.R.bool.config_intrusiveBatteryLed)) {
             prefScreen.removePreference(mChargingLeds);
         }
+
+        mEdgeLightColorPreference = (ColorPickerPreference) findPreference(PULSE_AMBIENT_LIGHT_COLOR);
+        int edgeLightColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF);
+        mEdgeLightColorPreference.setNewPreviewColor(edgeLightColor);
+        mEdgeLightColorPreference.setAlphaSliderEnabled(false);
+        String edgeLightColorHex = String.format("#%08x", (0xFF3980FF & edgeLightColor));
+        if (edgeLightColorHex.equals("#ff3980ff")) {
+            mEdgeLightColorPreference.setSummary(R.string.color_default);
+        } else {
+            mEdgeLightColorPreference.setSummary(edgeLightColorHex);
+        }
+        mEdgeLightColorPreference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mEdgeLightColorPreference) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#ff3980ff")) {
+                preference.setSummary(R.string.color_default);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PULSE_AMBIENT_LIGHT_COLOR, intHex);
+            return true;
+        }
+        return false;
     }
 
     @Override

TheXPerienceProject/android_packages_apps_RainbowUnicorn

Change-Id: Iae4a91764f97f79aa1d67b4361f3b4ba76e0d742
Signed-off-by: klozz <klozz@vivaldi.net>

src/mx/xperience/rainbowunicorn/fragments/NotificationSettings.java
@@ -42,11 +42,15 @@ import com.android.settings.R;
 
 import com.android.internal.util.xperience.XPerienceUtils;
 
+import mx.xperience.rainbowunicorn.colorpicker.ColorPickerPreference;
+
 public class NotificationSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
+    private static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
 
     private ListPreference mFlashlightOnCall;
+    private ColorPickerPreference mEdgeLightColorPreference;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -65,6 +69,19 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
          if (!XPerienceUtils.deviceHasFlashlight(getActivity())) {
             prefScreen.removePreference(FlashOnCall);
         }*/
+
+        mEdgeLightColorPreference = (ColorPickerPreference) findPreference(PULSE_AMBIENT_LIGHT_COLOR);
+        int edgeLightColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF);
+        mEdgeLightColorPreference.setNewPreviewColor(edgeLightColor);
+        mEdgeLightColorPreference.setAlphaSliderEnabled(false);
+        String edgeLightColorHex = String.format("#%08x", (0xFF3980FF & edgeLightColor));
+        if (edgeLightColorHex.equals("#ff3980ff")) {
+            mEdgeLightColorPreference.setSummary(R.string.default_string);
+        } else {
+            mEdgeLightColorPreference.setSummary(edgeLightColorHex);
+        }
+        mEdgeLightColorPreference.setOnPreferenceChangeListener(this);
     }
 
      public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -78,6 +95,19 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
                mFlashlightOnCall.setSummary(mFlashlightOnCall.getEntry());
                return true;
             }*/
+        if (preference == mEdgeLightColorPreference) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            if (hex.equals("#ff3980ff")) {
+                preference.setSummary(R.string.default_string);
+            } else {
+                preference.setSummary(hex);
+            }
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.PULSE_AMBIENT_LIGHT_COLOR, intHex);
+            return true;
+		}
         return false;
     }
 
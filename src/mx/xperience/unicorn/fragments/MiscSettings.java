5a20eac1c3224062a79cd4479cc3c288785c9e10
klozz <carlosj@klozz.dev>
2025-03-05 18:12:34 -0600

    add udfps icons and animation support
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/mx/xperience/unicorn/fragments/MiscSettings.java
+++ src/mx/xperience/unicorn/fragments/MiscSettings.java
@@ -3,20 +3,23 @@ package mx.xperience.unicorn.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.os.Bundle;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
-import android.content.Context;
-import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.hardware.fingerprint.FingerprintManager;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
-import mx.xperience.framework.preference.CustomSeekBarPreference;
 import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import java.util.Locale;
@@ -30,10 +33,11 @@ import java.util.Collection;
 import java.util.HashSet;
 
 import android.content.pm.PackageManager.NameNotFoundException;
-import com.android.settings.SettingsPreferenceFragment;
+import mx.xperience.framework.preference.CustomSeekBarPreference;
 import mx.xperience.framework.preference.SystemSettingMasterSwitchPreference;
 import mx.xperience.framework.preference.SystemSettingListPreference;
 import mx.xperience.framework.preference.SecureSettingSwitchPreference;
+import com.android.internal.util.voltage.VoltageUtils;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import android.provider.SearchIndexableResource;
@@ -44,6 +48,15 @@ import java.util.List;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String KEY_UDFPS_ICON = "udfps_icon";
+    private static final String KEY_ANIMATIONS_CATEGORY = "themes_animations_category";
+    private static final String KEY_UDFPS_ANIMATION = "udfps_animation";
+
+    private PreferenceCategory mIconsCategory;
+    private Preference mUdfpsIcon;
+    private PreferenceCategory mAnimationsCategory;
+    private Preference mUdfpsAnimation;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -62,10 +75,30 @@ public class MiscSettings extends SettingsPreferenceFragment implements
             e.printStackTrace();
         }
 
+        mUdfpsIcon = (Preference) findPreference(KEY_UDFPS_ICON);
+        mAnimationsCategory = (PreferenceCategory) findPreference(KEY_ANIMATIONS_CATEGORY);
+        mUdfpsAnimation = (Preference) findPreference(KEY_UDFPS_ANIMATION);
+
+        FingerprintManager fingerprintManager = (FingerprintManager)
+                getActivity().getSystemService(ctx.FINGERPRINT_SERVICE);
+
+        if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+            mIconsCategory.removePreference(mUdfpsIcon);
+            mAnimationsCategory.removePreference(mUdfpsAnimation);
+        } else {
+            if (!VoltageUtils.isPackageInstalled(ctx, "mx.xperience.udfps.animations")) {
+                mIconsCategory.removePreference(mUdfpsIcon);
+            }
+            if (!VoltageUtils.isPackageInstalled(ctx, "mx.xperience.udfps.animations")) {
+                mAnimationsCategory.removePreference(mUdfpsAnimation);
+            }
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
         return false;
     }
 
@@ -96,7 +129,24 @@ public class MiscSettings extends SettingsPreferenceFragment implements
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
+
+                final Resources resources = context.getResources();
+
+                FingerprintManager fingerprintManager = (FingerprintManager)
+                        context.getSystemService(Context.FINGERPRINT_SERVICE);
+
+                if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+                    keys.add(KEY_UDFPS_ICON);
+                    keys.add(KEY_UDFPS_ANIMATION);
+                } else {
+                    if (!VoltageUtils.isPackageInstalled(context, "mx.xperience.udfps.animations")) {
+                        keys.add(KEY_UDFPS_ICON);
+                    }
+                    if (!VoltageUtils.isPackageInstalled(context, "mx.xperience.udfps.animations")) {
+                        keys.add(KEY_UDFPS_ANIMATION);
+                    }
                 }
+                return keys;
+            }
     };
 }
